0c7c2f058f5bfdb26a65b936cde48663492405a3
2025-Nev <nevillenyax@gmail.com>
2023-02-17 23:12:22 -0800

    Data Structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-
-    uniq_num = []
-
-    for x in my_list:
-
-        if x not in uniq_num:
-
-            uniq_num.append(x)
-
-    result = 0
-
-    for x in uniq_num:
-
-        result += x
-
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
