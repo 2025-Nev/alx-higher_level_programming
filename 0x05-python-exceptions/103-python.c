ce34db50069d32cc715bd52991530c5b916eca7d
2025-Nev <nevillenyax@gmail.com>
2023-11-20 00:23:43 -0800

    high level language

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -14,9 +14,10 @@ void print_python_list(PyObject *p)
 	const char *type;
 	PyListObject *list = (PyListObject *)p;
 	PyVarObject *var = (PyVarObject *)p;
+
 	size = var->ob_size;
-	
 	alloc = list->allocated;
+
 	fflush(stdout);
 
 	printf("[*] Python list info\n");
@@ -25,7 +26,6 @@ void print_python_list(PyObject *p)
 		printf("  [ERROR] Invalid List Object\n");
 		return;
 	}
-
 
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", alloc);
@@ -51,20 +51,22 @@ void print_python_bytes(PyObject *p)
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	fflush(stdout);
+
 	printf("[.] bytes object info\n");
 	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
-
 	}
 
 	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
 	printf("  trying string: %s\n", bytes->ob_sval);
+
 	if (((PyVarObject *)p)->ob_size >= 10)
 		size = 10;
 	else
 		size = ((PyVarObject *)p)->ob_size + 1;
+
 	printf("  first %ld bytes: ", size);
 	for (i = 0; i < size; i++)
 	{
@@ -80,13 +82,15 @@ void print_python_bytes(PyObject *p)
  * print_python_float - Prints basic info about Python float objects.
  * @p: A PyObject float object.
  */
-
 void print_python_float(PyObject *p)
 {
 	char *buffer = NULL;
+
 	PyFloatObject *float_obj = (PyFloatObject *)p;
+
 	fflush(stdout);
-	printf("[.] float object info\n")
+
+	printf("[.] float object info\n");
 	if (strcmp(p->ob_type->tp_name, "float") != 0)
 	{
 		printf("  [ERROR] Invalid Float Object\n");
@@ -94,7 +98,6 @@ void print_python_float(PyObject *p)
 	}
 
 	buffer = PyOS_double_to_string(float_obj->ob_fval, 'r', 0,
-
 			Py_DTSF_ADD_DOT_0, NULL);
 	printf("  value: %s\n", buffer);
 	PyMem_Free(buffer);
